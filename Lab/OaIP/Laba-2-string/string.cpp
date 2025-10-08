eb4561c9cbeaccb465327691e2b5a50b31b191ce
Zabi1one <google228kk@mail.ru>
2025-10-08 08:32:10 +0300

    Добавил 1.txt

--- Lab/OaIP/Laba-2-string/string.cpp
+++ Lab/OaIP/Laba-2-string/string.cpp
@@ -1,26 +1,68 @@
 #include <iostream>
 #include <string>
-#include <fstream>
 #include <cstring>
-
 using namespace std;
 
 int main() {
-    char *stri[20];
-    const char search = ' ';
-    cout << "Введите строку из 10 слов: ";
+    string stroka;
+    string stroka1;
+    cout << "Введите 10 слов используя латинские символы: ";
+    getline(cin, stroka1);
 
-    cin.getline(stri, 20);
-    char strchr(stri, fin);
+    stroka = stroka1 + ' ';
+    
+    int probel = 0;
+    int probel1 = 0; // 3 задача
+    int i = 0;
+    int LastBukvaD = 0;
+    int b = 0; // 2 задача
+    int s = size(stroka);
+    int bukvaB = 0;
+    char findBuk = 'b';
+    //                                     1 Задача - Найти количество слов, оканчивающих на буквы b 
+    // for(int i = 0; i <= s; i++)
+    // {
+    //     if(stroka[i] == ' ') // Ищем пробелы, и делаем -1 (понимаем что это конец слова)
+    //     {
+    //         probel += 1;
+    //         if(stroka[i-1] == findBuk) // Цикл поска нужной буквы в конце слова
+    //         {
+    //             bukvaB += 1;
+    //         }
+    //     }
+    // }
+    
+    // cout << stroka << "\n" << s << "\n" << probel << "\n" << bukvaB;
 
-    char *result = strchr(stri, search); 
-    if (result != nullptr) { 
-    cout << "Символ '" << search << "' найден в строке: " << result << 
-    endl; 
-    } else { 
-        cout << "Символ '" << search << "' не найден в строке." << std::endl; 
-    } 
+    // //                                            2 задача - Найти длину самого длинного слова "Hello5Bro9my12name17is20Vlad25"
+    //     for(int i = 0; i <= s; i++)
+    // {
+    //     if(stroka[i] == ' ') 
+    //     {
+    //         int a = i - probel - 1;
+    //         probel = i;
+    //         if (b < a)
+    //         {
+    //             b = a;
+    //         }
+    //     }
+    // }
+    // cout << b << endl;
 
-    cout << "\nВывод: " << stri;
+        //                                            3 задача - Подсчитать количество букв d в последнем слове строки 
+        for(int i = 0; i < s; i++)
+        {
+            b = stroka1.rfind(' ');
+            for (b; b <= s; b++)
+            {
+                if (stroka[b] == 'd')
+                {
+                    LastBukvaD += 1;
+                }
+            }
+        }
+
+    cout << LastBukvaD << endl;
+
     return 0;
 }

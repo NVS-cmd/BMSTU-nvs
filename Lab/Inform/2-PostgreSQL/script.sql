c9641d73af53d95ce21f3cb3517f8c026f8db0a5
Zabi1one <google228kk@mail.ru>
2025-11-08 12:35:03 +0300

    Обновление SQL

--- Lab/Inform/2-PostgreSQL/script.sql
+++ Lab/Inform/2-PostgreSQL/script.sql
@@ -1,39 +1,58 @@
-CREATE TYPE priority AS ENUM ('Высокий', 'Средний', 'Низкий')
-CREATE TYPE status AS ENUM ('Выполнена', 'В процессе', 'Отложена')
+CREATE TYPE priority AS ENUM ('Высокий', 'Средний', 'Низкий');
+CREATE TYPE status AS ENUM ('Выполнена', 'В процессе', 'Отложена');
 CREATE TABLE tasks (
-	id SERIAL PRIMARY KEY,
+	id INT GENERATED ALWAYS AS IDENTITY (START WITH 100) PRIMARY KEY,
 	title VARCHAR (50) NOT NULL,
 	priority priority NOT NULL,
 	due_date DATE NOT NULL,
 	status status NOT NULL
 );
+
 CREATE TABLE users (
-	id SERIAL PRIMARY KEY,
+	id INT GENERATED ALWAYS AS IDENTITY (START WITH 100) PRIMARY KEY,
 	name VARCHAR (50)
 );
+
 CREATE TABLE task_assigments (
-	assigment_id SERIAL PRIMARY KEY,
-	FOREIGN KEY task_id INT REFERENCES tasks(id),
-	FOREIGN KEY user_id INT REFERENCES users(id)
+	assigment_id INT GENERATED ALWAYS AS IDENTITY (START WITH 1) PRIMARY KEY,
+	task_id INT,
+	user_id INT,
+	FOREIGN KEY (task_id) REFERENCES tasks(id),
+	FOREIGN KEY (user_id) REFERENCES users(id)
 );
+
 INSERT INTO users (name)
-	VALUES (Vlad),
-	VALUES (Alex),
-	VALUES (Vasiliy);
+VALUES ('Vlad'),
+	('Alex'),
+	('Vasiliy');
+
 INSERT INTO tasks (title, priority, due_date, status)
-	VALUES ('Сделать отчёт', 'Средний', '07.10.2025', 'Выполнена'),
-	VALUES ('Переустановить ОС', 'Низкий', '10.12.2025', 'Отложена'),
-	VALUES ('Сделать резервные копии', 'Высокий', '20.11.2025', 'В процессе'),
-	VALUES ('Настроить Базу данных', 'Низкий', '22.10.2025', 'Выполнена'),
-	VALUES ('Подготовить презентацию', 'Средний', '10.09.2025', 'Выполнена'),
-	VALUES ('Написать статью', 'Высокий', '20.11.2025', 'В процессе');
-INSERT INTO task_assigments (task_id, user_id)
-	VALUES (1,2),
-	VALUES (2,4),
-	VALUES (3,6),
-	VALUES (1,5),
-	VALUES (2,3),
-	VALUES (3,1);
+VALUES ('Сделать отчёт', 'Средний', '07.10.2025', 'Выполнена'),
+	('Переустановить ОС', 'Низкий', '10.12.2025', 'Отложена'),
+	('Сделать резервные копии', 'Высокий', '20.11.2025', 'В процессе'),
+	('Настроить Базу данных', 'Низкий', '22.10.2025', 'Выполнена'),
+	('Подготовить презентацию', 'Средний', '10.09.2025', 'Выполнена'),
+	('Написать статью', 'Высокий', '20.11.2025', 'В процессе');
+
+INSERT INTO task_assigments (user_id, task_id)
+VALUES (101,102),
+	(102,104),
+	(100,100),
+	(101,105),
+	(102,103),
+	(100,101);
 
 
+Запросы:
+SELECT (title, priority, due_date) FROM tasks;
 
+SELECT t.title, t.priority, t.due_date, t.status
+FROM tasks t
+JOIN task_assigments ta ON t.id = ta.task_id
+WHERE ta.user_id = 101;
+
+SELECT (title, priority, due_date) FROM tasks WHERE priority = 'Высокий' AND due_date < '2025-11-19';
+
+SELECT status, COUNT(*) AS task_count
+FROM tasks
+GROUP BY status;
